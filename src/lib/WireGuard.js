80df5bb4fb8d91c3c03389c80e9a92c17a60fce9
shiboka <d.aubie@outlook.com>
2025-02-02 18:57:30 -0400

    pleaseunderstand

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -120,7 +120,7 @@ PostDown = ${WG_POST_DOWN}
 [Peer]
 PublicKey = ${client.publicKey}
 ${client.preSharedKey ? `PresharedKey = ${client.preSharedKey}\n` : ''
-}AllowedIPs = ${client.address}/32`;
+}AllowedIPs = ${WG_ALLOWED_IPS}`;
     }
 
     debug('Config saving...');
